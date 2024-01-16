0189f8dbf900025e5d325867990f522dc8ff1dc6
DIBYA RANJAN SAHOO <78527632+idibyaranjan15@users.noreply.github.com>
2024-01-16 06:49:24 +0000

    all about logical or control flow by hitesh chai aur code

---
+++ 04_basics/4.2.js
@@ -0,0 +1,31 @@
+//ternary opearator
+// const balance = 4000
+// balance >6700 ? console.log(`you are amir`) : console.log(`you are poor`)
+
+
+// switch in javascript
+
+// const month=2
+
+// switch(month){
+//     case 1:
+//         console.log(`1st month`);
+//         break;
+
+//         case 2:
+//             console.log(`2nd month`);
+//             break;
+//             case 3 :
+//                 console.log(`3rd month`)
+//                 break;
+// }
+
+// ?? opearator(nullish coalescing opearator)
+let val1;
+val1 = null ?? 10;
+console.log(val1)
+let val2=10 ?? 23 ?? 34;
+console.log(val2)
+let val3=10??null??34;
+console.log(val3)
+
