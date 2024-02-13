a9a26a6bc9a04ac4fb8fd06e644774b2e972b804
DIBYA RANJAN SAHOO <78527632+idibyaranjan15@users.noreply.github.com>
2024-02-13 11:28:39 +0000

    Dom  Manipulation

---
+++ 09_basics/9.index.js
@@ -0,0 +1,37 @@
+// // let resultedElement=document.querySelectorAll('p')
+// // console.log(resultedElement)
+// // let resultedClass=document.querySelector('h2')
+// // console.log(resultedClass)
+
+// // let getTagNames=document.getElementsByTagName('p')
+// // console.log(getTagNames)
+// let content=document.querySelector('.content')
+// content.innerHTML +="<h3> hello coder part2 </h3>"
+// // console.log(content.innerHTML)
+
+
+// // console.log(content.innerText)
+
+
+// let googleLink=document.querySelector('a')
+// googleLink.setAttribute('href',"https://www.amazon.in")
+// // console.log(googleLink.getAttribute('href')
+// googleLink.innerText="AMAZON"
+
+// let changeHandle=document.querySelector('h1')
+
+// changeHandle.setAttribute('id',"firstHeading")
+// console.log(changeHandle.getAttribute('id'))
+// changeHandle.innerText='Namaste Family'
+
+
+
+
+// let heading=document.querySelectorAll('h2')
+// // heading.style.backgroundColor='green'
+// // heading.style.padding='10px'
+
+// heading.forEach((e)=>{
+//     e.classList.replace('cd',"coderdost")
+//     e.innerText="Hello World!"
+// })
